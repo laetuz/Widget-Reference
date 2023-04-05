8d4a65030abacbc395db6d18972c1cb6aa3d6cd1
Ryo Martin <laetuzg@gmail.com>
2023-04-05 20:55:22 +0700

    Initial commit

---
+++ app/src/main/java/com/neotica/widgetreference/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.neotica.widgetreference
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neotica.widgetreference.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
